721336b135e090f6e86366b20888751d642a7a24
ashugautamtradechbh-cloud <ashugautamtradechbh@gmail.com>
2026-07-19 21:20:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,4 @@
-import nest_asyncio
-nest_asyncio.apply()
-
-from pyrogram import Client
-
-bot = Client("MahiruMasterBot", 
-             api_id=32467601, 
-             api_hash="4c4f019c3eacb6794ec5cdcc8030595e", 
-             bot_token="8859491458:AAGKjcuFvWQxBpj61mMGZbRTWu3XafbCutw",
-             sleep_threshold=10)
-
-print("Bot starting...")
-bot.run()
+import os
+api_id = os.getenv("32467601")
+api_hash = os.getenv("4c4f019c3eacb6794ec5cdcc8030595e")
+bot_token = os.getenv("8547370233:AAESbzo5igxQHLCUZTON8K1c2eg82xLsqkw")
